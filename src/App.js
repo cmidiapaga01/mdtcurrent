1065e72033b2efc7cebaeaed00a9a396d7d77c16
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2024-08-07 00:24:56 +0200

    maoe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-MZBZQDJ5'
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
