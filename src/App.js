81cfadf74c9e70c0b8e9117e94c824eef42efaa3
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2024-08-07 10:50:38 +0200

    Descrição das mudanças

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to fffload.
         </p>
         <a
           className="App-link"
